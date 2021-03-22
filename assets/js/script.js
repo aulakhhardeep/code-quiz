ae49b68d5ad98fde4fa24f9e1f2bb4e24eae4288
Hardeep <aulakh.hardeep91@gmail.com>
2021-03-21 20:15:35 -0400

    Added array of questions

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        question:"1. Which of the following is the correct syntax to print a page using JavaScript?",
+        option: ["1. window.print();", "2. browser.print();", "3. navigator.print();", "4.  document.print();"],
+        answer: "1. window.print()"
+    },
+    {
+        question: "2. Which event occurs when the user clicks on an HTML element?",
+        option: ["1. onmouseover", "2. onchange", "3. onclick", "4. onmouseclick"],
+        answer: "3. onclick"
+    },
+
+    {
+       question: "3. What are variables used for in JavaScript Programs",
+       option: ["1. Storing numbers, dates, or other values", "2. Varying randomly", "3. Causing high-school algebra flashbacks", 
+       "4. None of the above"],
+       answer: "1. Storing numbers, dates, or other values"
+    },
+
+    {
+        question: "4. Choose the client-side JavaScript object?",
+        option: ["1. Database", "2. Cursor", "3. Client", "4. FileUpload"],
+        answer: "4. FileUpload"
+    },
+    {
+        question: "5. Using _______ statement is how you test for a specific condition.",
+        option: ["1. Select", "2. If", "3. Switch", "4. For"],
+        answer: "2. If"
+    }
+
+
+]
